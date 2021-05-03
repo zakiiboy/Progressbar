84c6ba288533b1c786731f110b4935e15c70de7e
zakaria el anigri <83065832+zakiiboy@users.noreply.github.com>
2021-05-03 12:00:16 +0000

    Update code.py

--- code.py
+++ code.py
@@ -5,3 +5,9 @@ with Bar('Progress:', fill='#', suffix='%(percent).1f%% Complete') as bar:
     for i in range(100):
         sleep(0.02)
         bar.next()
+*************************************************************************************************************************************************
+from tqdm import tqdm
+x=1
+for i in tqdm(range(0,100000)):
+    for x in range(0,1000):
+        x*=5
